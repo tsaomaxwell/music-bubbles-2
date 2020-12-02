14e1dfd95126a2a8eb76e06b6759dc1c6db017c2
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2020-12-01 19:16:07 -0800

    skeleton for small bubbles, no looks or logic yet

---
+++ music_bubbles/src/components/SmallBubble.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './SmallBubble.css';
+
+function SmallBubble(){
+    return <div className = 'outer'>
+        <div className = 'name'>
+            Pop
+        </div>
+    </div>
+}
+
+
+export default SmallBubble;
