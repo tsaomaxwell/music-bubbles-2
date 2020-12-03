73e77580dd7d628e02657bdd8be4c2316d29c42c
tsaomaxwell <68205375+tsaomaxwell@users.noreply.github.com>
2020-12-02 18:48:05 -0800

    turned child into button to change state

--- music_bubbles/src/components/BigBubble.js
+++ music_bubbles/src/components/BigBubble.js
@@ -1,15 +1,27 @@
-import React, {  } from 'react';
+import React, { useState } from 'react'
+//mport ReactDOM from 'react-dom'
+//import CSVReader from 'react-csv-reader'
 import './BigBubble.css';
 import SmallBubble from './SmallBubble.js'
 
 
+
 function BigBubble(props){
-    const parentsName = props.parent; // update later to array handling
-    const childOneName = props.childOne;// update later for array handling
+    const [parentsName, setParentsName] = useState(props.parent); // update later to array handling
+    const [childOneName, setChildOneName] = useState(props.childOne);// update later for array handling
     
+    function getBubble(props){
+        setParentsName("Classical");
+        setChildOneName("Impressionist")
+        console.log("clicked");
+    
+        props.info = "It's all in the name. 1830-1900"
+    }
+
     return <div className = "Big">
         <SmallBubble
             title={parentsName}
+            onClick={getBubble}
         />
 
         <div className = 'inner'>
@@ -29,9 +41,11 @@ function BigBubble(props){
 
         
 
-        <SmallBubble
+        <button className='container' onClick={getBubble}>
+            <SmallBubble
             title={childOneName}
-        />
+            />
+        </button>
     </div>
 
 
